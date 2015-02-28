54ffd87281bfe714496537d2d7f4ee695a4b24e4
古映杰 <yj_gu@Ctrip.com>
2015-02-28 12:15:32 +0800

    save

--- jplus2/lib/zoom.js
+++ jplus2/lib/zoom.js
@@ -69,6 +69,14 @@
 		}
 	}, false)
 
+	if (doc.readyState === 'complete') {
+        doc.body.style.fontSize = 12 * dpr + 'px';
+    } else {
+        doc.addEventListener('DOMContentLoaded', function(e) {
+            doc.body.style.fontSize = 12 * dpr + 'px';
+        }, false);
+    }
+
 	setRootFontSize()
 
 }(window));
