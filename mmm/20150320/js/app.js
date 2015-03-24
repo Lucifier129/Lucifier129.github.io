4d8f6ab2da89eecffa6be7bbdec9229c5352bc2a
Jade <guyingjie129@163.com>
2015-03-24 22:19:50 +0800

    test19

--- mmm/20150320/js/app.js
+++ mmm/20150320/js/app.js
@@ -150,9 +150,11 @@ $(function() {
 
 	var $goToForm = $('[data-role="goToForm"]')
 	var $submit = $('.form-area [type="submit"]')
+	var $mask = $('.mask')
 
 	FastClick.attach($goToForm[0])
 	FastClick.attach($submit[0])
+	FastClick.attach($mask[0])
 
 
 	$goToForm.on('click', function() {
