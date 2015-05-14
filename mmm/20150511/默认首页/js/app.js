527f12f2beef3a8e579d35eef931cba6e1b4c959
Jade <guyingjie129@163.com>
2015-05-14 22:57:27 +0800

    save

---
+++ mmm/20150511/默认首页/js/app.js
@@ -0,0 +1,11 @@
+$(function() {
+	//dom ready
+
+
+
+// '日语50音   日语能力考    生活日语      日剧动漫      韩国旅游      生活韩语       韩语TOPIK  韩版跑男 日语旅游    德语入门      法语入门      学法语绕不过的1200词     现代西班牙    实用西班牙   泰国旅游'
+// .split(/\s+/)
+// .map(function(word) { 
+// return '<dd><a href="javascript" target="_blank">word</a></dd>'.replace('word', word)
+// }).join('\n')
+})
