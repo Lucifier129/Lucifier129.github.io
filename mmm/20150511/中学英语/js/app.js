5b84e8849481a7c554c706b8b64b05e56d5542d5
Jade <guyingjie129@163.com>
2015-05-17 20:13:07 +0800

    update

---
+++ mmm/20150511/中学英语/js/app.js
@@ -0,0 +1,81 @@
+;
+(function($) {
+
+    $.fn.tab = function(options) {
+        if (!options || !options.target) {
+            throw new Error('$.fn.tab need a param about target selector')
+        }
+        if (!$.isArray(options.target)) {
+            var selectorList = [options.target]
+        } else {
+            var selectorList = options.target
+        }
+        var change = function() {
+            var $this = $(this)
+            var index = $this.index()
+            $this
+                .addClass('active')
+                .siblings('.active').removeClass('active')
+            $.each(selectorList, function(_, selector) {
+                var $target = $(selector).hide().eq(index).show()
+                if ($.isFunction(options.callback)) {
+                    options.callback($target, index)
+                }
+            })
+        }
+        var timer
+        var type = options.type || 'mouseenter'
+        this.on(type, options.src || 'li', function() {
+            var that = this
+            timer = setTimeout(function() {
+                change.call(that)
+            }, 100)
+        }).on('mouseleave', 'li', function() {
+            clearTimeout(timer)
+        })
+        this.find(options.src || 'li').eq(0).trigger(type)
+    }
+}(jQuery))
+
+$(function() {
+	//dom ready
+	$('.left-side-content .height01 .tab-list').tab({
+		target: '.left-side-content .height01 .the-content',
+		src: 'a'
+	})
+
+
+	$('.img-player.fl .img-pager').tab({
+		target: '.img-player.fl .img-inner a',
+		src: 'span',
+		type: 'click'
+	})
+
+	$('.img-player.fr .img-pager').tab({
+		target: '.img-player.fr .img-inner a',
+		src: 'span',
+		type: 'click'
+	})
+
+
+	$('.img-player').each(function() {
+		var $elem = $(this)
+		var next = function() {
+			var $children = $elem.find('.img-pager').children()
+			var index = $elem.find('.img-pager .active').index()
+			index = (index + 1) % $children.length
+			$children.eq(index).trigger('click')
+		}
+		var timer
+		function addTimer() {
+			timer = setInterval(next, 3000)
+		}
+		$elem.on('mouseenter', '.img-pager', function() {
+			clearInterval(timer)
+		}).on('mouseleave', '.img-pager', addTimer)
+		addTimer()
+	})
+
+
+
+})
--- mmm/20150511/默认首页/js/app.js
+++ mmm/20150511/默认首页/js/app.js
@@ -1,11 +1,106 @@
+;
+(function($) {
+
+    $.fn.tab = function(options) {
+        if (!options || !options.target) {
+            throw new Error('$.fn.tab need a param about target selector')
+        }
+        if (!$.isArray(options.target)) {
+            var selectorList = [options.target]
+        } else {
+            var selectorList = options.target
+        }
+        var change = function() {
+            var $this = $(this)
+            var index = $this.index()
+            $this
+                .addClass('active')
+                .siblings('.active').removeClass('active')
+            $.each(selectorList, function(_, selector) {
+                var $target = $(selector).hide().eq(index).show()
+                if ($.isFunction(options.callback)) {
+                    options.callback($target, index)
+                }
+            })
+        }
+        var timer
+        this.on('mouseenter', options.src || 'li', function() {
+            var that = this
+            timer = setTimeout(function() {
+                change.call(that)
+            }, 100)
+        }).on('mouseleave', 'li', function() {
+            clearTimeout(timer)
+        })
+        this.find(options.src || 'li').eq(0).trigger('mouseenter')
+    }
+}(jQuery))
+
 $(function() {
-	//dom ready
+    //dom ready
+
+    $('.slideshow').each(function() {
+        var slider = $(this).bxSlider({
+            controls: false,
+            touchEnabled: false,
+            auto: true,
+            autoHover: true,
+            onSlideAfter: function() {
+                slider.stopAuto()
+                slider.startAuto()
+            }
+        })
+        $(this).data('slider', slider)
+    })
+
+    $('.tab-slider').each(function() {
+    	var slider = $(this).bxSlider({
+	        slideWidth: 114,
+	        minSlides: 3,
+	        maxSlides: 30,
+	        moveSlides: 3,
+	        slideMargin: 12,
+	        pager: false,
+	        touchEnabled: false
+	    })
+	    $(this).data('slider', slider)
+    })
+
+    $('.tab-of-slider .tab-header').tab({
+        target: '.tab-of-slider .tab-slider-content',
+        callback: function($target) {
+        	var slider = $target.find('.tab-slider').data('slider')
+        	if (slider) {
+        		slider.reloadSlider()
+        	}
+        }
+    })
+
+    $('.tab01 .tab-list').tab({
+        target: '.tab01 .list01',
+        src: 'a'
+    })
+
+    $('.tab02 .tab-list').tab({
+        target: '.tab02 .list01',
+        src: 'a'
+    })
+
+    $('.english-skill .tab-list').tab({
+    	target: ['.english-skill .main-english-skill', '.english-skill .links-header .fr'],
+    	src: 'a'
+    })
+
+    $('.learn-english .tab-list').tab({
+        target: '.learn-english .main-learn-english',
+        src: 'a',
+        callback: function($target, selector) {
+            var slider = $target.find('.slideshow').data('slider')
+            if (slider) {
+                slider.reloadSlider()
+            }
+        }
+    })
 
 
-
-// '日语50音   日语能力考    生活日语      日剧动漫      韩国旅游      生活韩语       韩语TOPIK  韩版跑男 日语旅游    德语入门      法语入门      学法语绕不过的1200词     现代西班牙    实用西班牙   泰国旅游'
-// .split(/\s+/)
-// .map(function(word) { 
-// return '<dd><a href="javascript" target="_blank">word</a></dd>'.replace('word', word)
-// }).join('\n')
 })
